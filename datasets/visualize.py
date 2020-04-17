ff6c09e6a94836d5136a4c4422cfe719db6b1c65
Enes Demirag <ensdmrg@gmail.com>
2020-04-17 12:42:31 +0300

    visualizing

---
+++ datasets/visualize.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("../datasets/california-housing-dataset.csv")
+
+df["median_house_value"] /= 500001
+
+longitude = list(df["longitude"])
+latitude = list(df["latitude"])
+prices = list(df["median_house_value"])
+
+plt.scatter(x = longitude, y = latitude, s=4, c = prices, cmap='rainbow')
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.show()
